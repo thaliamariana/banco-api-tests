9e682efe6e7b17840778333d30313b93ceb2a058
Thalía Mariana <thaliamarianamartins@gmail.com>
2026-09-21 17:41:53 -0300

    Adicionando a biblioteca .env para var globais

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,11 @@
-const request = require('supertest');
+const request = require('supertest')
 const { expect } = require('chai')
+require('dotenv').config()
 
 describe('Login', ()=>{
     describe('POST/login', ()=>{
         it('Deve retornar 200 com o token em string quando usar credenciais validas', async ()=>{
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest')
 const  { expect } = require('chai')
+require('dotenv').config()
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor for igual ou maior que R$10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -14,7 +15,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('Content-Type', 'application/json')
               .set('Authorization', `Bearer ${token}`)
@@ -29,7 +30,7 @@ describe('Transferências', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor for menor que R$10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -39,7 +40,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('Content-Type', 'application/json')
               .set('Authorization', `Bearer ${token}`)
